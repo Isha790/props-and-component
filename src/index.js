4a57ba512a10d7deed58166027463923dff28694
Isha Khatri <khatriisha79@gmail.com>
2023-01-29 09:48:09 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Card from './Card';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+const cards = [
+{
+className: 'card-blue',
+imgSrc: 'https://images.unsplash.com/photo-1524593689594-aae2f26b75ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+imgAlt: 'Blue card image',
+title: 'Card 1',
+description: 'This is the first card.'
+},
+{
+className: 'card-green',
+imgSrc: 'https://images.unsplash.com/photo-1524593689594-aae2f26b75ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+imgAlt: 'Green card image',
+title: 'Card 2',
+description: 'This is the second card.'
+},
+{
+className: 'card-red',
+imgSrc: 'https://images.unsplash.com/photo-1524593689594-aae2f26b75ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+imgAlt: 'Red card image',
+title: 'Card 3',
+description: 'This is the third card.'
+}
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+return (
+<div>
+{cards.map((card, index) => {
+return <Card key={index} {...card} />
+})}
+</div>
+)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
